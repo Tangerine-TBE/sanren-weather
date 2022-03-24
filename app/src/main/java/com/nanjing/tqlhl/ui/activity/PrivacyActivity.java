b868a16f954a37a34c3e1fc7f61c461df62f64c1
yf <colijn_ye@163.com>
2022-03-24 15:42:34 +0800

    长按复制隐私政策

--- app/src/main/java/com/nanjing/tqlhl/ui/activity/PrivacyActivity.java
+++ app/src/main/java/com/nanjing/tqlhl/ui/activity/PrivacyActivity.java
@@ -1,9 +1,13 @@
 package com.nanjing.tqlhl.ui.activity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.nanjing.tqlhl.base.BaseMainActivity;
 import com.nanjing.tqlhl.utils.PackageUtil;
@@ -35,6 +39,15 @@ public class PrivacyActivity extends BaseMainActivity {
         tv_bar_title.setVisibility(View.VISIBLE);
         tv_bar_title.setText(getString(R.string.user_Privacy));
         mWordAlignTextView.setText(PackageUtil.difPlatformName(this,R.string.privacy));
+        mWordAlignTextView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                ClipboardManager clipboardManager=(ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                clipboardManager.setPrimaryClip(ClipData.newPlainText("隐私政策",mWordAlignTextView.getText()));
+                Toast.makeText(PrivacyActivity.this,"复制成功",Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
     }
 
     @Override
